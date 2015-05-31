fd71728aefe780819c623a48a9474a0f98447245
Josh Lauer <jlauer79@gmail.com>
2015-05-30 19:31:07 -0700

    Formatting files

--- html/src/com/fivem/rts/client/HtmlLauncher.java
+++ html/src/com/fivem/rts/client/HtmlLauncher.java
@@ -7,13 +7,13 @@ import com.fivem.rts.SpaceRTS;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
+  @Override
+  public GwtApplicationConfiguration getConfig() {
+    return new GwtApplicationConfiguration(480, 320);
+  }
 
-        @Override
-        public ApplicationListener getApplicationListener () {
-                return new SpaceRTS();
-        }
+  @Override
+  public ApplicationListener getApplicationListener() {
+    return new SpaceRTS();
+  }
 }
