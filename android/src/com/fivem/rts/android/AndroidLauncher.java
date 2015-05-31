3169b7ee0f21f980953de5600e8378bc25e81380
Daniel Lam <lamdanielalex@gmail.com>
2015-05-30 18:05:49 -0700

    Initial project commit.

---
+++ android/src/com/fivem/rts/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.fivem.rts.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.fivem.rts.SpaceRTS;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceRTS(), config);
+	}
+}
