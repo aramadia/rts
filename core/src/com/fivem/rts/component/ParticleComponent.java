177c581c236c73635d97a0ca88a48a2f2f91eb99
Josh Lauer <jlauer79@gmail.com>
2015-06-13 19:07:50 -0700

    Making bullets bigger

--- core/src/com/fivem/rts/component/ParticleComponent.java
+++ core/src/com/fivem/rts/component/ParticleComponent.java
@@ -3,6 +3,6 @@ package com.fivem.rts.component;
 import com.badlogic.ashley.core.Component;
 
 public class ParticleComponent extends Component {
-  public float size = 3.0f;
+  public float size = 10.0f;
 
 }
