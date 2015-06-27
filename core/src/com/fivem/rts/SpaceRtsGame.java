9294b345cef544427f06cdf0bd237d1c251a52d9
Daniel Lam <lamdanielalex@gmail.com>
2015-06-26 17:30:31 -0700

    Refactored World
    World contains methods to initialize entities

--- core/src/com/fivem/rts/SpaceRtsGame.java
+++ core/src/com/fivem/rts/SpaceRtsGame.java
@@ -42,6 +42,7 @@ public class SpaceRtsGame extends ApplicationAdapter {
   private NetworkManager networkManager;
   public static Random random;
   private SpriteBatch spriteBatch;
+  private World world;
 
   public SpaceRtsGame(GoogleServicesInterface googleServicesInterface, NetworkManager networkManager){
     this.googleServicesInterface = googleServicesInterface;
@@ -57,6 +58,7 @@ public class SpaceRtsGame extends ApplicationAdapter {
     cameraHud = new OrthographicCamera();
     viewport = new FitViewport(SCENE_WIDTH, SCENE_HEIGHT, camera);
     viewportHud = new FillViewport(SCENE_WIDTH, SCENE_HEIGHT, cameraHud);
+    world = new World();
 
     // Center camera
     viewport.getCamera().position.set(
@@ -94,95 +96,7 @@ public class SpaceRtsGame extends ApplicationAdapter {
     ashleyEngine.addSystem(consoleSystem);
     ashleyEngine.addSystem(commandWriteSystem);
 
-
-    for (int i = 0; i < 3; i++) {
-      Entity entity = createSmileyEntity(i);
-      ashleyEngine.addEntity(entity);
-    }
-
-    for (int i = 0; i < 50; i++) {
-      Entity zombie = createZombie(i);
-      ashleyEngine.addEntity(zombie);
-    }
-  }
-
-  private Entity createZombie(int i) {
-    Entity entity = new Entity();
-
-    AnimatedComponent animation = new AnimatedComponent();
-    TransformComponent transform = new TransformComponent();
-    BoundsComponent bounds = new BoundsComponent();
-    MovementComponent movement = new MovementComponent();
-    TextComponent text = new TextComponent();
-    SelectionComponent selection = new SelectionComponent();
-    ZombieComponent zombie = new ZombieComponent();
-
-    int width = 100;
-    int height = 100;
-
-    text.text = "Zombie" + i;
-
-    animation.sheet = new Texture(Gdx.files.internal("zombiewalk.png")); // #9
-    TextureRegion[][] tmp = TextureRegion.split(animation.sheet, animation.sheet.getWidth()/8,
-        animation.sheet.getHeight()/1);              // #10
-    animation.frames = new TextureRegion[8 * 1];
-    int index = 0;
-    for (int y = 0; y < 1; y++) {
-      for (int x = 0; x < 8; x++) {
-        animation.frames[index++] = tmp[y][x];
-      }
-    }
-    animation.animation = new Animation(0.1f, animation.frames);
-    transform.position.set(SCENE_WIDTH * random.nextFloat() - width * .4f,
-        SCENE_HEIGHT * random.nextFloat() - height * .4f, 0);
-    transform.scale.set(0.5f, 0.5f);
-    bounds.setBoundsFromRect(transform.position.x, transform.position.y, width, height);
-    movement.velocity.set(100, 0).setAngleRad(MathUtils.PI2 * random.nextFloat());
-
-    entity.add(animation);
-    entity.add(transform);
-    entity.add(bounds);
-    entity.add(movement);
-    entity.add(text);
-    entity.add(selection);
-    entity.add(zombie);
-
-    return entity;
-  }
-
-  private Entity createSmileyEntity(int i) {
-    Entity entity = new Entity();
-
-    TextureComponent texture = new TextureComponent();
-    TransformComponent transform = new TransformComponent();
-    BoundsComponent bounds = new BoundsComponent();
-    MovementComponent movement = new MovementComponent();
-    TextComponent text = new TextComponent();
-    GunnerComponent gunner = new GunnerComponent();
-    SelectionComponent selection = new SelectionComponent();
-
-    int width = 100;
-    int height = 100;
-
-    text.text = "Entity" + i;
-
-    texture.region = new TextureRegion(new Texture(Gdx.files.internal("badlogic.jpg")));
-    transform.position.set(SCENE_WIDTH * random.nextFloat() - width * .4f, SCENE_HEIGHT * random.nextFloat() - height * .4f, 0);
-    bounds.setBoundsFromRect(transform.position.x, transform.position.y, width, height);
-    movement.velocity.set(10, 10);
-    movement.acceleration.set(30, 30);
-
-    gunner.reload_progress = random.nextFloat() * gunner.reload_time;
-
-    entity.add(texture);
-    entity.add(transform);
-    entity.add(bounds);
-    entity.add(movement);
-    entity.add(text);
-    entity.add(gunner);
-    entity.add(selection);
-
-    return entity;
+    world.initWorld(ashleyEngine);
   }
 
   @Override
--- core/src/com/fivem/rts/World.java
+++ core/src/com/fivem/rts/World.java
@@ -1,8 +1,108 @@
 package com.fivem.rts;
 
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.fivem.rts.component.*;
+
 /**
  * Contains helpers to initialize the world.
  * Created by Daniel on 6/25/2015.
  */
 public class World {
+  public void initWorld(Engine engine) {
+    for (int i = 0; i < 3; i++) {
+      Entity entity = createSmileyEntity(i);
+      engine.addEntity(entity);
+    }
+
+    for (int i = 0; i < 50; i++) {
+      Entity zombie = createZombie(i);
+      engine.addEntity(zombie);
+    }
+  }
+
+  private Entity createZombie(int i) {
+    Entity entity = new Entity();
+
+    AnimatedComponent animation = new AnimatedComponent();
+    TransformComponent transform = new TransformComponent();
+    BoundsComponent bounds = new BoundsComponent();
+    MovementComponent movement = new MovementComponent();
+    TextComponent text = new TextComponent();
+    SelectionComponent selection = new SelectionComponent();
+    ZombieComponent zombie = new ZombieComponent();
+
+    int width = 100;
+    int height = 100;
+
+    text.text = "Zombie" + i;
+
+    animation.sheet = new Texture(Gdx.files.internal("zombiewalk.png")); // #9
+    TextureRegion[][] tmp = TextureRegion.split(animation.sheet, animation.sheet.getWidth()/8,
+        animation.sheet.getHeight()/1);              // #10
+    animation.frames = new TextureRegion[8 * 1];
+    int index = 0;
+    for (int y = 0; y < 1; y++) {
+      for (int x = 0; x < 8; x++) {
+        animation.frames[index++] = tmp[y][x];
+      }
+    }
+    animation.animation = new Animation(0.1f, animation.frames);
+    transform.position.set(SpaceRtsGame.SCENE_WIDTH * SpaceRtsGame.random.nextFloat() - width * .4f,
+        SpaceRtsGame.SCENE_HEIGHT * SpaceRtsGame.random.nextFloat() - height * .4f, 0);
+    transform.scale.set(0.5f, 0.5f);
+    bounds.setBoundsFromRect(transform.position.x, transform.position.y, width, height);
+    movement.velocity.set(100, 0).setAngleRad(MathUtils.PI2 * SpaceRtsGame.random.nextFloat());
+
+    entity.add(animation);
+    entity.add(transform);
+    entity.add(bounds);
+    entity.add(movement);
+    entity.add(text);
+    entity.add(selection);
+    entity.add(zombie);
+
+    return entity;
+  }
+
+  private Entity createSmileyEntity(int i) {
+    Entity entity = new Entity();
+
+    TextureComponent texture = new TextureComponent();
+    TransformComponent transform = new TransformComponent();
+    BoundsComponent bounds = new BoundsComponent();
+    MovementComponent movement = new MovementComponent();
+    TextComponent text = new TextComponent();
+    GunnerComponent gunner = new GunnerComponent();
+    SelectionComponent selection = new SelectionComponent();
+
+    int width = 100;
+    int height = 100;
+
+    text.text = "Entity" + i;
+
+    texture.region = new TextureRegion(new Texture(Gdx.files.internal("badlogic.jpg")));
+    transform.position.set(SpaceRtsGame.SCENE_WIDTH * SpaceRtsGame.random.nextFloat() - width * .4f,
+        SpaceRtsGame.SCENE_HEIGHT * SpaceRtsGame.random.nextFloat() - height * .4f, 0);
+    bounds.setBoundsFromRect(transform.position.x, transform.position.y, width, height);
+    movement.velocity.set(10, 10);
+    movement.acceleration.set(30, 30);
+
+    gunner.reload_progress = SpaceRtsGame.random.nextFloat() * gunner.reload_time;
+
+    entity.add(texture);
+    entity.add(transform);
+    entity.add(bounds);
+    entity.add(movement);
+    entity.add(text);
+    entity.add(gunner);
+    entity.add(selection);
+
+    return entity;
+  }
 }
